db4070d8c8e6456fcd7254f11831fbe780959682
Kenji Miura <miura@nextgen.co.jp>
2020-07-26 16:07:20 +0900

    Initial Commit

---
+++ ios_audio_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_audio_test
+//
+//  Created by Kenji Miura on 2020/07/26.
+//  Copyright © 2020 Kenji Miura. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
